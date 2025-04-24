2a97490bade88914e81305978a03ff24eca78ce5
Brian Rabil <brianrabil@mac.com>
2025-04-24 11:38:53 -0500

    responsive fixes

--- components/header.tsx
+++ components/header.tsx
@@ -3,16 +3,29 @@
 import { Container } from "@/components/container";
 import { ModeToggle } from "@/components/mode-toggle";
 import { SearchInput } from "@/components/search-input";
-import { Button } from "@/components/ui/button";
+import {
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTrigger,
+} from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
-import { Menu, X } from "lucide-react";
+import { Store, useStore } from "@tanstack/react-store";
+import { MenuIcon } from "lucide-react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Separator } from "./ui/separator";
+
+const store = new Store({
+	isScrolled: false,
+	isMobileMenuOpen: false,
+});
+
+const navItems = [{ name: "Explore collection", href: "/search" }];
 
 export function Header() {
-	const [isScrolled, setIsScrolled] = useState(false);
-	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const isScrolled = useStore(store, (state) => state.isScrolled);
 	const pathname = usePathname();
 	const isHomePage = pathname === "/";
 	const isSearchPage = pathname === "/search";
@@ -20,9 +33,15 @@ export function Header() {
 	useEffect(() => {
 		const handleScroll = () => {
 			if (window.scrollY > 10) {
-				setIsScrolled(true);
+				store.setState((state) => ({
+					...state,
+					isScrolled: true,
+				}));
 			} else {
-				setIsScrolled(false);
+				store.setState((state) => ({
+					...state,
+					isSCrolled: false,
+				}));
 			}
 		};
 
@@ -57,14 +76,11 @@ export function Header() {
 					<div className="hidden md:flex items-center space-x-6">
 						<nav>
 							<ul className="flex space-x-6">
-								{[
-									{ name: "Explore Collection", href: "/search" },
-									// { name: "Departments", href: "/departments" },
-									// { name: "About", href: "/about" },
-								].map((item) => (
+								{navItems.map((item) => (
 									<li key={item.name}>
 										<Link
 											href={item.href}
+											prefetch
 											className={cn(
 												"text-sm font-medium transition-colors hover:text-primary",
 												isScrolled || !isHomePage
@@ -81,7 +97,7 @@ export function Header() {
 						</nav>
 
 						{/* Desktop Search */}
-						<div className="flex gap-x-2">
+						<div className="gap-x-2 hidden md:flex">
 							<SearchInput isTransparent={!isScrolled && isHomePage} />
 							<div className="opacity-75">
 								<ModeToggle
@@ -98,57 +114,92 @@ export function Header() {
 
 					{/* Mobile Menu Button */}
 					<div className="flex items-center md:hidden">
-						<ModeToggle />
-						<SearchInput isTransparent={!isScrolled && isHomePage} />
-						<Button
-							variant="ghost"
-							size="icon"
-							className={cn(
-								"ml-2",
-								isScrolled || !isHomePage
-									? "text-foreground"
-									: "text-white hover:text-white/80 hover:bg-white/10",
-							)}
-							onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-							aria-label="Toggle menu"
-						>
-							{isMobileMenuOpen ? (
-								<X className="h-5 w-5" />
-							) : (
-								<Menu className="h-5 w-5" />
-							)}
-						</Button>
+						<MobileMenu />
 					</div>
 				</div>
 			</Container>
+		</header>
+	);
+}
 
-			{/* Mobile Menu */}
-			{isMobileMenuOpen && (
-				<div className="md:hidden bg-background border-b">
-					<nav className="container mx-auto px-4 py-4">
-						<ul className="space-y-4">
-							{[
-								{ name: "Explore collection", href: "/search" },
-								// { name: "Departments", href: "/departments" },
-								// { name: "About", href: "/about" },
-							].map((item) => (
-								<li key={item.name}>
+function MobileMenu() {
+	const isScrolled = useStore(store, (state) => state.isScrolled);
+	const pathname = usePathname();
+	const isHomePage = pathname === "/";
+
+	return (
+		<Sheet>
+			<SheetTrigger
+				className={cn(
+					"ml-2",
+					isScrolled || !isHomePage
+						? "text-foreground"
+						: "text-white hover:text-white/80 hover:bg-white/10",
+				)}
+			>
+				<MenuIcon className="h-5 w-5" />
+			</SheetTrigger>
+			<SheetContent>
+				<SheetHeader>
+					<Link href="/" className="flex items-center">
+						<span
+							className={cn(
+								"font-serif text-2xl font-bold tracking-tight transition-colors text-foreground",
+							)}
+						>
+							Meet the Met
+						</span>
+					</Link>
+				</SheetHeader>
+				<Container>
+					<div className="flex flex-col">
+						<div className="flex items-center gap-x-2 flex-nowrap">
+							<SearchInput isTransparent={!isScrolled && isHomePage} />
+							<div className="opacity-75">
+								<ModeToggle
+									className={cn(
+										isScrolled || !isHomePage
+											? "text-foreground"
+											: "text-white",
+										"hover:text-primary",
+									)}
+								/>
+							</div>
+						</div>
+						<Separator className="mb-4 mt-8" />
+						<nav className="container mx-auto px-4 py-4">
+							<ul className="space-y-4">
+								<li>
 									<Link
-										href={item.href}
+										href="/"
+										prefetch
 										className={cn(
-											"block text-lg font-medium",
-											pathname === item.href ? "font-semibold" : "",
+											"block font-medium",
+											pathname === "/" ? "font-semibold" : "",
 										)}
-										onClick={() => setIsMobileMenuOpen(false)}
 									>
-										{item.name}
+										Home
 									</Link>
 								</li>
-							))}
-						</ul>
-					</nav>
-				</div>
-			)}
-		</header>
+								{navItems.map((item) => (
+									<li key={item.name}>
+										<Link
+											href={item.href}
+											prefetch
+											className={cn(
+												"block font-medium",
+												pathname === item.href ? "font-semibold" : "",
+											)}
+										>
+											{item.name}
+										</Link>
+									</li>
+								))}
+							</ul>
+						</nav>
+					</div>
+				</Container>
+			</SheetContent>
+		</Sheet>
 	);
 }
